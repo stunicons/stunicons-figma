0756ce923ba7f51a02d77e5d5ded3d5d93928c3d
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-24 15:02:12 +0200

    feat: using vue for plugin development

--- src/code.ts
+++ src/code.ts
@@ -1,82 +1,33 @@
-figma.showUI(__html__)
-figma.ui.onmessage = async (numbers) => {
-  // Roboto Regular is the font that objects will be created with by default in
-  // Figma. We need to wait for fonts to load before creating text using them.
-  await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
-  const frameWidth = 800
-  const frameHeight = 600
-  const chartX = 25
-  const chartY = 50
-  const chartWidth = frameWidth - 50
-  const chartHeight = frameHeight - 50
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser enviroment (see documentation).
 
-  
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
 
-  // const frame = figma.createFrame()
-  const frame = figma.currentPage
-  // frame.resizeWithoutConstraints(frameWidth, frameHeight)
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'create-rectangles') {
+    const nodes: SceneNode[] = [];
+    for (let i = 0; i < msg.count; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
+    }
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
 
-  // Center the frame in our current viewport so we can see it.
-  // frame.x = figma.viewport.center.x - frameWidth / 2
-  // frame.y = figma.viewport.center.y - frameHeight / 2
-  const svg = figma.createNodeFromSvg('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M13 19h6V9.978l-7-5.444-7 5.444V19h6v-6h2v6zm8 1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.49a1 1 0 0 1 .386-.79l8-6.222a1 1 0 0 1 1.228 0l8 6.222a1 1 0 0 1 .386.79V20z"/></svg>')
-
- 
-  frame.appendChild(svg)
-
-  // Border for the chart
-  // const border = figma.createRectangle()
-  // frame.appendChild(border)
-  // border.resizeWithoutConstraints(frameWidth, frameHeight)
-  // border.strokeAlign = 'INSIDE'
-  // border.strokeWeight = 3
-  // border.fills = []
-  // border.strokes = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-  // border.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-
-  // Line at the bottom of the chart
-  // const line = figma.createRectangle()
-  // frame.appendChild(line)
-  // line.x = chartX
-  // line.y = chartY + chartHeight
-  // line.resizeWithoutConstraints(chartWidth, 3)
-  // line.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-  // line.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-
-  // const min = numbers.reduce((a, b) => Math.min(a, b), 0)
-  // const max = numbers.reduce((a, b) => Math.max(a, b), 0)
-
-  // for (let i = 0; i < numbers.length; i++) {
-    // const num = numbers[i];
-    // const left = chartX + chartWidth * (i + 0.25) / numbers.length;
-    // const right = chartX + chartWidth * (i + 0.75) / numbers.length;
-    // const top = chartY + chartHeight - chartHeight * (Math.max(0, num) - min) / (max - min);
-    // const bottom = chartY + chartHeight - chartHeight * (Math.min(0, num) - min) / (max - min);
-
-    // The column
-    // const column = figma.createRectangle()
-    // frame.appendChild(column)
-    // column.x = left
-    // column.y = top
-    // column.resizeWithoutConstraints(right - left, bottom - top)
-    // column.fills = [{ type: 'SOLID', color: {r: 1, g: 0, b: 0} }]
-    // column.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-
-    // The label
-    // const label = figma.createText()
-    // frame.appendChild(label)
-    // label.x = left - 50
-    // label.y = top - 50
-    // label.resizeWithoutConstraints(right - left + 100, 50)
-    // label.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-    // label.characters = num.toString()
-    // label.fontSize = 30
-    // label.textAlignHorizontal = 'CENTER'
-    // label.textAlignVertical = 'BOTTOM'
-    // label.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-
-  // }
-
-  figma.closePlugin()
-}
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
