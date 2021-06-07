93105f810f3b268e3ee808ebd824daf4b973305a
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-08 00:29:57 +0200

    feat: getting all icons

---
+++ src/ui/components/Icons-list.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="icons-list">
+        <section class="icons-list--wraper">
+            <div v-for="iconGroup in icons" :key="iconGroup.categoryName">
+              <div v-for="icon in iconGroup.icons" :key="icon.id"> 
+                  <Icon name="icon.id"/>
+              </div>
+            </div>
+        </section>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {icons as _icons} from '../services/icons.json'
+import Icon from './Icon.vue';
+
+
+
+export default defineComponent({
+    setup() {
+        const icons = _icons;
+        console.log(icons)
+        return { icons };
+    },
+    components: { Icon },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
