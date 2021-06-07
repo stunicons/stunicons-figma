bfaea109161f6d64eb28fb7f6bfc32a47543eb13
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-08 01:25:24 +0200

    feat: set default height of the plugin

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,16 @@
 import { dispatch, handleEvent } from './codeMessageHandler';
-figma.showUI(__html__);
+
+const options: ShowUIOptions = {
+    height: 400,
+};
+
+figma.showUI(__html__, options);
 
 // The following shows how messages from the UI code can be handled in the main code.
 handleEvent('createNode', () => {
-	const node = figma.createRectangle();
-	node.name = node.id;
+    const node = figma.createRectangle();
+    node.name = node.id;
 
-	// This shows how the main code can send messages to the UI code.
-	dispatch('nodeCreated', node.id);
+    // This shows how the main code can send messages to the UI code.
+    dispatch('nodeCreated', node.id);
 });
