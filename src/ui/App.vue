f3d7d59e89955353558647a9ade250f58c51ede2
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-07 22:58:38 +0200

    chore: using global configured scss

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,25 +1,32 @@
 <template>
 <div id="ui">
   
-  <div id="searchBox">
-    <i class="si-search" id="searchIcon"></i>
-      <input type="text" placeholder="Search an icon">
-    <i class="si-exit exitIcon"></i>
-</div>
+  <div class="search">
+    <Search v-model="searchString" />
+  </div>
+
 
 </div>
 </template>
 
 <script lang='ts'>
+import { defineComponent, Ref, ref } from "vue";
+import Search from "./components/Search.vue";
 
+export default defineComponent({
+    components: { Search },
+    setup(){
+      let searchString: Ref<string> = ref('')
 
-export default {
-
-};
+      return {
+        searchString
+      }
+    }
+});
 </script>
 
-<style scoped>
-/* @import url('https://cdn.jsdelivr.net/npm/stunicons@1.0.11/fonts/style.min.css'); */
+<style scoped lang="scss">
+
 .btn{
   background: #3234;
 }
@@ -28,47 +35,9 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  padding: var(--size-medium);
-}
-
-#searchBox{
-  padding: 10px;
-  background:#FFF0EA;
-  display: flex;
-  align-items: center;
-  height: 15px;
-
-}
-#searchIcon{
-  width:50px;
-  cursor: pointer;
-  color: #BFC7D3;
-  font-size: 14px;
-}
-.exitIcon{
-  border-radius: 20px;
-  color: #BFC7D3;
-  cursor: pointer;
-  background: #ffffff;
-  font-size: 9px;
-  padding: 5px;
-}
-.menuIcon{
-  width: 20px;
-  color: #BFC7D3;
-  font-size: 15px;
-}
-input{
-  font-size: 10px;
-  width: 150px;
-  height: 20px;
-  border: 0;
-  background: transparent;
-  outline: none;
- 
-}
-
-::placeholder{
-  color: #BFC7D3;
+  
+  .search{
+    padding:$s-sm;
+  }
 }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -5,99 +5,103 @@ const { VueLoaderPlugin } = require('vue-loader');
 const path = require('path');
 
 module.exports = (env, argv) => ({
-	// This is necessary because Figma's 'eval' works differently than normal eval
-	devtool: argv.mode === 'production' ? false : 'inline-source-map',
+    // This is necessary because Figma's 'eval' works differently than normal eval
+    devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
-	entry: {
-		ui: './src/ui/main.ts', // The entry point for your UI code
-		code: './src/code.ts' // The entry point for your plugin code
-	},
+    entry: {
+        ui: './src/ui/main.ts', // The entry point for your UI code
+        code: './src/code.ts', // The entry point for your plugin code
+    },
 
-	module: {
-		rules: [
-			// Converts TypeScript code to JavaScript
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-				options: {
-					appendTsSuffixTo: [/\.vue$/]
-				}
-			},
+    module: {
+        rules: [
+            // Converts TypeScript code to JavaScript
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
 
-			// Enables including CSS by doing "import './file.css'" in your TypeScript code
-			{ test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							implementation: require('sass')
-						}
-					}
-				]
-			},
+            // Enables including CSS by doing "import './file.css'" in your TypeScript code
+            { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            additionalData: '@import "src/ui/assets/scss/main.scss";',
+                            sassOptions: {
+                                includePaths: [__dirname, 'src'],
+                            },
+                        },
+                    },
+                ],
+            },
 
-			{
-				test: /\.pug$/,
-				loader: 'pug-plain-loader'
-			},
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader',
+            },
 
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			},
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
 
-			// Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-			{ test: /\.(png|jpg|gif|webp)$/, loader: [{ loader: 'url-loader' }] },
+            // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
+            { test: /\.(png|jpg|gif|webp)$/, loader: [{ loader: 'url-loader' }] },
 
-			//load fonts
-			{ 
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, 
-				loader:'url-loader' ,
-			},
-		]
-	},
+            //load fonts
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+            },
+        ],
+    },
 
-	// Webpack tries these extensions for you if you omit the extension like "import './file'"
-	resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js',] },
+    // Webpack tries these extensions for you if you omit the extension like "import './file'"
+    resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
 
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist') // Compile into a folder called "dist"
-	},
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    },
 
-	// Tells Webpack to generate "index.html" and to inline "main.ts" into it
-	plugins:
-		argv.mode === 'production'
-			? [
-					new VueLoaderPlugin(),
-					new RemovePlugin({
-						after: { include: ['dist/ui.js'] }
-					}),
-					new HtmlWebpackPlugin({
-						template: './src/ui/index.html',
-						filename: 'index.html',
-						inlineSource: '.(js|css|scss)$',
-						chunks: ['ui']
-					}),
-					new HtmlWebpackInlineSourcePlugin()
-			  ]
-			: [
-					new VueLoaderPlugin(),
-					new HtmlWebpackPlugin({
-						template: './src/ui/index.html',
-						filename: 'index.html',
-						inlineSource: '.(js|css|scss)$',
-						chunks: ['ui']
-					}),
-					new HtmlWebpackInlineSourcePlugin()
-			  ]
+    // Tells Webpack to generate "index.html" and to inline "main.ts" into it
+    plugins:
+        argv.mode === 'production'
+            ? [
+                  new VueLoaderPlugin(),
+                  new RemovePlugin({
+                      after: { include: ['dist/ui.js'] },
+                  }),
+                  new HtmlWebpackPlugin({
+                      template: './src/ui/index.html',
+                      filename: 'index.html',
+                      inlineSource: '.(js|css|scss)$',
+                      chunks: ['ui'],
+                  }),
+                  new HtmlWebpackInlineSourcePlugin(),
+              ]
+            : [
+                  new VueLoaderPlugin(),
+                  new HtmlWebpackPlugin({
+                      template: './src/ui/index.html',
+                      filename: 'index.html',
+                      inlineSource: '.(js|css|scss)$',
+                      chunks: ['ui'],
+                  }),
+                  new HtmlWebpackInlineSourcePlugin(),
+              ],
 });
