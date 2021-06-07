61d85e74dfda128271dd8412b94bc4cc68c51758
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-08 00:50:33 +0200

    style: single icon style

--- src/ui/components/Icon.vue
+++ src/ui/components/Icon.vue
@@ -21,6 +21,8 @@ export default defineComponent({
 <style lang="scss" scoped>
 .icon{
     padding:$s-md;
+    border-radius:1px;
+    cursor:pointer;
     
     &:hover{
         background-color:#F0F0F0;
