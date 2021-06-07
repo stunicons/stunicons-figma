284412d937efb85ba14162b1067de88099883a5d
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-07 22:59:01 +0200

    vue configs

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: `@import "@src/ui/assets/scss/main.scss";`,
+            },
+        },
+    },
+};
