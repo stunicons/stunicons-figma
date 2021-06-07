da3de2fddaa154f2869827501b55f6bf31099daf
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-07 23:22:41 +0200

    feat: icon

---
+++ src/ui/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="icon">
+        <div class="icon-w">
+            <i :class="className"></i>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props:{
+        className:{
+            type:String,
+            default:''
+        }
+    },
+    setup() {
+        
+    },
+})
+</script>
