596f3e75f38661bc40d974bf3e796b5689d9ebcc
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-14 23:16:26 +0200

    perf: node compatability

--- src/ui/utils/svgToEl.ts
+++ src/ui/utils/svgToEl.ts
@@ -1,3 +1,10 @@
-export default function dataUriToSvg(dataUri: string): string {
-    return atob(dataUri.replace(/data:image\/svg\+xml;base64,/, ''));
+export async function dataUriToSvg(dataUri: string): Promise<string> {
+    const g: typeof globalThis = window || global;
+    const b64: string = dataUri.replace(/data:image\/svg\+xml;base64,/, '');
+
+    if (g.atob) {
+        return await g.atob(b64);
+    } else {
+        return await Buffer.from(b64, 'base64').toString('utf8');
+    }
 }
